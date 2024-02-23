d5abde1f19c3c13673df5725c068cdbdffac1614
oovaa <omarvx211@gmail.com>
2024-02-23 12:35:12 +0200

    task 1.3 at 23-12

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,6 @@
 # install_flask
 exec { 'install_flask':
-  command     => '/usr/bin/pip3 install Flask==2.1.0',
-  path        => '/usr/bin',
+  command     => '/usr/bin/pip3 install "Flask==2.1.0"',
+  path        => ['/usr/bin'],
   refreshonly => true,
 }
-
