660d159051a7880d2ff67d6b4b82fd53cddcbb3d
oovaa <omarvx211@gmail.com>
2024-02-23 12:00:40 +0200

    puppet tasks till 2 at 23-12

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,7 @@
+file { '/tmp/school':
+  ensure  => 'file',      # Ensure it's a file
+  mode    => '0744',      # Set file permissions
+  owner   => 'www-data',  # Set file owner
+  group   => 'www-data',  # Set file group
+  content => 'I love Puppet',  # Set file content
+}
---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,5 @@
+# Install puppet-lint
+package { 'flask':
+  ensure   => '3.0.2',
+  provider => 'pip3'
+}
