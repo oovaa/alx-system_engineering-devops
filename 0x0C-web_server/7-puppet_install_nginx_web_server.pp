b67b4509f5074a50fd54afd3d1945796ffe3d60a
oovaa <omarvx211@gmail.com>
2024-02-27 22:02:58 +0200

    update task 5 at 27-22

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -5,20 +5,34 @@ exec { 'update system':
 }
 
 package { 'nginx':
-	ensure => 'installed',
-	require => Exec['update system']
+    ensure => 'installed',
+    require => Exec['update system']
+}
+
+file {'/etc/nginx/sites-available/default':
+    content => '
+server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+
+    location / {
+        return 200 "Hello World!\n";
+    }
+
+    location /redirect_me {
+        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+    }
+}
+',
+    require => Package['nginx']
 }
 
 file {'/var/www/html/index.html':
-	content => 'Hello World!'
-}
-
-exec {'redirect_me':
-	command => 'sed -i "10i\	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
-	provider => 'shell'
+    content => 'Hello World!',
+    require => Package['nginx']
 }
 
 service {'nginx':
-	ensure => running,
-	require => Package['nginx']
+    ensure => running,
+    require => Package['nginx']
 }
