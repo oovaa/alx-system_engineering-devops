856112edbff336aa40915dbf66f00132dea7df1c
oovaa <omarvx211@gmail.com>
2024-03-05 23:30:20 +0200

    task 2.2 at 05-23

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,27 +1,27 @@
-# Puppet manifest for a configuration file in the server
 # execute 'apt-get update'
 exec { 'apt-update':
-	command => '/usr/bin/apt update'
+  command => '/usr/bin/apt update'
 }
 
 # install nginx package
 package { 'nginx':
-	require => Exec['apt-update'],
-	ensure  => installed,
+  require => Exec['apt-update'],
+  ensure  => installed,
 }
 
-file { '/var/www/htmlindex.html':
-	ensure  => file,
-	content => 'Hello World!',
-	require => Package['nginx']
+file { '/var/www/html/index.html':
+  ensure  => file,
+  content => 'Hello World!',
+  require => Package['nginx']
 }
 
-exec { 'Redirection':
-	provider => shell,
-	command  => 'sudo sed -i "s#server_name _;#server_name _;\n        location /redirect_me {\n                rewrite ^/redirect_me/?$ https://www.youtube.com/watch?v=dQw4w9WgXcQ permanent;\n        }#" /etc/nginx/sites-available/default ; sudo service nginx restart',
+file { '/etc/nginx/sites-available/default':
+  ensure  => file,
+  content => template('nginx/default.erb'),
+  notify  => Service['nginx'],
 }
 
-exec { 'custom_header':
-	provider => shell,
-	command  => 'hostname=$(hostname) ; string="\tadd_header X-Served-By \"$hostname\";" ; sudo sed -i "/server_name _;/ a\\ $string\n" /etc/nginx/sites-available/default ; sudo service nginx restart',
+service { 'nginx':
+  ensure => running,
+  enable => true,
 }
