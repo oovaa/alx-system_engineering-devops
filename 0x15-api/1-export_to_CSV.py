c542e2077ead02c6127eaaae7516316f271b740f
oovaa <omarvx211@gmail.com>
2024-03-25 22:48:24 +0200

    tasks 1,2 at 25-22

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,42 +1,23 @@
 #!/usr/bin/python3
-"""
-This module fetches data from the JSONPlaceholder API.
-It retrieves the tasks for a specific user and writes them to a CSV file.
-"""
-
-import csv
-import requests
-from sys import argv
-
-
-def fetch_data_and_write_to_csv(employee_id):
-    """
-    Fetches data from the JSONPlaceholder API for a specific user.
-    Writes the user's tasks to a CSV file.
-    """
-
-    # Get the employee's details
-    name_res = requests.get(
-        "https://jsonplaceholder.typicode.com/users/{}".
-        format(employee_id))
-    name = name_res.json().get('name')  # Use get to access dictionary value
-
-    # Get the employee's tasks
-    tasks_res = requests.get(
-        "https://jsonplaceholder.typicode.com/todos?userId={}".
-        format(employee_id))
-    tasks = tasks_res.json()
-
-    # Open the CSV file and write the tasks to it
-    with open('{}.csv'.format(employee_id), 'w', newline='') as csvfile:
-        taskwriter = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        for task in tasks:
-            taskwriter.writerow(
-                [employee_id, name, task.get('completed'), task.get('title')])
-
+"""Exports data in the CSV format"""
 
 if __name__ == "__main__":
-    # Only run the following code when the script is executed directly
-    # (not imported)
-    employee_id = argv[1]
-    fetch_data_and_write_to_csv(employee_id)
+
+    import csv
+    import requests
+    import sys
+
+    userId = sys.argv[1]
+    user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                        .format(userId))
+    name = user.json().get('username')
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
+
+    filename = userId + '.csv'
+    with open(filename, mode='w') as f:
+        writer = csv.writer(f, delimiter=',', quotechar='"',
+                            quoting=csv.QUOTE_ALL, lineterminator='\n')
+        for task in todos.json():
+            if task.get('userId') == int(userId):
+                writer.writerow([userId, name, str(task.get('completed')),
+                                 task.get('title')])
--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,51 +1,29 @@
 #!/usr/bin/python3
-"""
-This module fetches data from the JSONPlaceholder API.
-It retrieves the tasks for a specific user
-and writes them to a JSON file.
-The file is named after the user's ID and contains
-their tasks in a specific format.
-"""
-
-import json
-import requests
-from sys import argv
-
-
-def fetch_data(employee_ID):
-    """
-    Fetches data from the JSONPlaceholder API for a specific user.
-    Writes the user's tasks to a JSON file in a specific format.
-    """
-
-    # Get the employee's details
-    name_res = requests.get(
-        "https://jsonplaceholder.typicode.com/users/{}".
-        format(employee_ID))
-    name = name_res.json().get('name')  # Use get to access
-    # dictionary value
-
-    # Get the employee's tasks
-    tasks_res = requests.get(
-        "https://jsonplaceholder.typicode.com/todos?userId={}".
-        format(employee_ID))
-    tasks = tasks_res.json()
-
-    # Create JSON data to be written to the file
-    json_data = {
-        employee_ID: [{"task": task.get('title'), "completed":
-                       task.get(
-            'completed'), "username": name} for task in tasks]
-    }
-
-    # Write JSON data to file
-    with open('{}.json'.
-              format(employee_ID), 'w') as json_file:
-        json.dump(json_data, json_file)
-
+"""Exports data in the JSON format"""
 
 if __name__ == "__main__":
-    # Only run the following code when the script
-    # is executed directly (not imported)
-    employee_ID = argv[1]
-    fetch_data(employee_ID)
+
+    import json
+    import requests
+    import sys
+
+    userId = sys.argv[1]
+    user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                        .format(userId))
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
+    todos = todos.json()
+
+    todoUser = {}
+    taskList = []
+
+    for task in todos:
+        if task.get('userId') == int(userId):
+            taskDict = {"task": task.get('title'),
+                        "completed": task.get('completed'),
+                        "username": user.json().get('username')}
+            taskList.append(taskDict)
+    todoUser[userId] = taskList
+
+    filename = userId + '.json'
+    with open(filename, mode='w') as f:
+        json.dump(todoUser, f)
