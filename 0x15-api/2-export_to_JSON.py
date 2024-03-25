7345a2715f29d42a03ce1b4ba866d70703bfc190
oovaa <omarvx211@gmail.com>
2024-03-25 21:41:21 +0200

    refactor task 2 at 25-21

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,41 +1,43 @@
 #!/usr/bin/python3
 """
-task 1 APIs
-the api: https://jsonplaceholder.typicode.com/
-
+This module fetches data from the JSONPlaceholder API.
+It retrieves the tasks for a specific user and writes them to a JSON file.
+The file is named after the user's ID and contains their tasks in a specific format.
 """
+
+import requests
 import json
 from sys import argv
-import requests
 
-employee_ID = argv[1]
 
-name_res = requests.get("https://jsonplaceholder.typicode.com/users/{}".
-                        format(employee_ID))
+def fetch_data(employee_ID):
+    """
+    Fetches data from the JSONPlaceholder API for a specific user.
+    Writes the user's tasks to a JSON file in a specific format.
+    """
 
-name = name_res.json()['name']
+    # Get the employee's details
+    name_res = requests.get(
+        "https://jsonplaceholder.typicode.com/users/{}".format(employee_ID))
+    name = name_res.json().get('name')  # Use get to access dictionary value
 
-tasks_res = requests.get(
-    "https://jsonplaceholder.typicode.com/todos?userId={}".
-    format(employee_ID))
-tasks = tasks_res.json()
+    # Get the employee's tasks
+    tasks_res = requests.get(
+        "https://jsonplaceholder.typicode.com/todos?userId={}".format(employee_ID))
+    tasks = tasks_res.json()
 
-# Calculate the number of done tasks and the total number of tasks
-done_tasks = [task for task in tasks if task['completed']]
-number_of_done_tasks = len(done_tasks)
-total_number_of_tasks = len(tasks)
+    # Create JSON data to be written to the file
+    json_data = {
+        employee_ID: [{"task": task.get('title'), "completed": task.get(
+            'completed'), "username": name} for task in tasks]
+    }
 
-print("Employee {} is done with tasks({}/{}):".
-      format(name, number_of_done_tasks, total_number_of_tasks))
-for task in done_tasks:
-    print("\t {}".format(task['title']))
+    # Write JSON data to file
+    with open('{}.json'.format(employee_ID), 'w') as json_file:
+        json.dump(json_data, json_file)
 
-# Create JSON data to be written to the file
-json_data = {
-    employee_ID: [{"task": task['title'], "completed": task['completed'],
-                   "username": name} for task in tasks]
-}
 
-# Write JSON data to file
-with open('{}.json'.format(employee_ID), 'w') as json_file:
-    json.dump(json_data, json_file)
+if __name__ == "__main__":
+    # Only run the following code when the script is executed directly (not imported)
+    employee_ID = argv[1]
+    fetch_data(employee_ID)
